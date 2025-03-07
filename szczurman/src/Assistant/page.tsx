128bb8bfd34fa777e0cce282fb44a89ad6d4d31d
sh1n666 <m.jacoszek01@gmail.com>
2025-03-07 23:42:06 +0100

    fix router

--- szczurman/src/Assistant/page.tsx
+++ szczurman/src/Assistant/page.tsx
@@ -1,23 +1,17 @@
 import { MemoryRouter as Router, Routes, Route, Link } from "react-router-dom"
 
-function OptionsPage() {
-  return (
-    <div style={{ padding: 16 }}>
-      <h2>Options Page</h2>
-      <p>Here you can configure your settings.</p>
-      <Link to="../">
-        <button>Back to Home</button>
-      </Link>
-    </div>
-  )
+function AssistantPage() {
+
 }
 
-export default function Options() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="../" element={<OptionsPage />} />
-      </Routes>
-    </Router>
-  )
-}
+export default function Assistant() {
+    return (
+        <div style={{ padding: 16 }}>
+          <h2>Options Page</h2>
+          <p>Here you can configure your settings.</p>
+          <Link to="../">
+            <button>Back to Home</button>
+          </Link>
+        </div>
+      )
+  }
