07e4a53a9380d4253df64aa7b6d8ad6db7494f7d
PeterSolution <piotrwolff@op.pl>
2025-03-08 03:55:49 +0100

    FinalBtn

--- szczurman/src/popup.tsx
+++ szczurman/src/popup.tsx
@@ -2,32 +2,25 @@ import { MemoryRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Assistant from "./Assistant/page";
 import ListBox from "./PasswordManager/page";
 import PassFun from "./Options/page"
+import './HelpingFile.css';
+
 
 function Navbar() {
   return (
     <div style={{width: '180px'}}>
-      <nav className="bg-gradient-to-r from-teal-400 to-blue-500 p-4 shadow-lg">
-        <div className="max-w-7xl mx-auto flex justify-between items-center">
-          
-          <div >
-            <Link to="/Assistant">
-              <button className="flex px-6 py-3 bg-gradient-to-r from-yellow-400 to-red-500 text-white text-lg font-semibold rounded-lg transform transition duration-300 hover:scale-110 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-yellow-300">
-                Assystent
-              </button>
-            </Link>
-          
-            <Link to="/PasswordManager">
-              <button className="flex px-6 py-3 w-[120%] bg-gradient-to-r from-yellow-400 to-red-500 text-white text-lg font-semibold rounded-lg transform transition duration-300 hover:scale-110 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-yellow-300">
-                Hasla
-              </button>
-            </Link>
+      <nav>
+        <div className="btn-group">
+          <a href="/Assistant">
+            Assystent
+          </a>
 
-            <Link to="/Options">
-              <button className="flex px-6 py-3 w-[120%] bg-gradient-to-r from-yellow-400 to-red-500 text-white text-lg font-semibold rounded-lg transform transition duration-300 hover:scale-110 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-yellow-300">
-                Opcje
-              </button>
-            </Link>
-          </div>
+          <a href="/PasswordManager">
+            Hasla
+          </a>
+
+          <a href="/Options">
+            Opcje
+          </a>
         </div>
       </nav>
     </div>
